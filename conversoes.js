3aad1571d1cb0b7ab123b1f854d04ffe5bfc09a3
Lucas <lucasl_pereira@hotmail.com>
2023-01-12 12:05:56 -0300

    codigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,30 @@
+// tipo de dado
+// booleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString);
+
+// java converteu e virou iguais
+
+console.log(numero == numeroString);
+
+console.log(numero + numeroString);
+
+// um = "atribui", dois == "compara apenas o valor", tres === "compara valor e tipo"
+
+
+// conversao explicita
+
+// Number()
+
+// String()
+
+console.log(numero + Number(numeroString));
+
+// Se a String não conter apenas números resultará em NaN na conversão
+
+
+
